cc59423fe71816574f2e70d0da72b41ce5f0d53f
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-08-19 21:16:43 +0000

    exerci 2 continuar

--- ExercicioFor1.java
+++ ExercicioFor1.java
@@ -1,10 +1,25 @@
-// 1 Imprimir todos os números de 150 a 300
+// Exercicio For de 1 até 5
 public class ExercicioFor1{
     public static void main (String args[]){
-        for (int valor=150; valor<=300; valor++){
-            System.out.println(valor);
+        /*int n=0;
+        for (n=150; n<=300; n++){
+            //1. Imprimir todos os números de 150 a 300
+            System.out.println(n);
+        }*/
+        //2. Imprima a soma de 1 até 1000.
+        int n2=0;
+        for (n2=+1; n2>=100; n2++){
+            System.out.println("Soma de 1 até 100"+n2);
         }
 
     }
 
 }
+/*
+3. Imprima todos os múltiplos de 3, entre 1 e 100.
+4. Imprima os fatoriais de 1 a 10.
+5. (opcional) Escreva um programa que, dada uma variável x com algum valor inteiro, temos um
+novo x de acordo com a seguinte regra:
+se x é par, x = x / 2
+se x é impar, x = 3 * x + 1
+imprime x8*/
