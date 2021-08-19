04d623b1f4e70dced517286ec90a31b76fc3e088
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-08-19 19:35:56 +0000

    For

---
+++ ExercicioFor1.java
@@ -0,0 +1,10 @@
+// Imprimir todos os números de 150 a 300
+public class ExercicioFor1{
+    public static void main (String args[]){
+        for (int valor=150; valor<=300; valor++){
+            System.out.println(valor);
+        }
+
+    }
+
+}
